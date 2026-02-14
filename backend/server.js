c8d296f1b51615785949b56f3929d338c14a5b90
Abdul Kadir <abdulkadir80949@gmail.com>
2026-02-14 08:47:10 +0530

    Fix case sensitivity for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectdb from "./config/db.js";
 import cors from "cors";
+
+import connectdb from "./config/db.js";
+
 
 // Routes
 import authRoutes from "./routes/authRoutes.js";
